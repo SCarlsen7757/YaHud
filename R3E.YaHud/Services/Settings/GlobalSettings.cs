e8f9cc08091cb24f86df4d0196a59c133c9807d7
Mark Carlsen <mark@scarlsen.dk>
2025-10-18 22:06:24 +0200

    Added unit converter + Updated MoTec widget

--- R3E.YaHud/Services/Settings/GlobalSettings.cs
+++ R3E.YaHud/Services/Settings/GlobalSettings.cs
@@ -1,7 +1,13 @@
-﻿namespace R3E.YaHud.Services.Settings
+﻿using R3E.API;
+
+namespace R3E.YaHud.Services.Settings
 {
     public class GlobalSettings
     {
         public SettingsViewMode ViewMode { get; set; } = SettingsViewMode.Beginner;
+        public SpeedUnit SpeedUnit { get; set; } = SpeedUnit.KilometersPerHour;
+        public AngularUnit AngularUnit { get; set; } = AngularUnit.RevolutionsPerMinute;
+        public TemperatureUnit TemperatureUnit { get; set; } = TemperatureUnit.Celsius;
+
     }
 }
