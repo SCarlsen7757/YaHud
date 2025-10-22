4fec546e2ae8efc115de822295182a4cc9420504
Oliver <Oliver.veile@gmail.com>
2025-10-22 23:22:14 +0200

    Added FuelFolder and made calculations for fuel widget (needs review)

---
+++ R3E.YaHud/Components/Widget/FuelGauge/FuelSettings.cs
@@ -0,0 +1,19 @@
+﻿using R3E.YaHud.Components.Widget.Core;
+using R3E.YaHud.Services.Settings;
+
+namespace R3E.YaHud.Components.Widget.FuelGauge
+{
+    public class FuelSettings : BasicSettings
+    {
+        [SettingType("Fuel Green", SettingsTypes.ColorPicker, 10,
+            Description = "Color of the RPM bar",
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string RpmColor { get; set; } = "#0069ff";
+        
+
+        [SettingType("RPM Redline Color", SettingsTypes.ColorPicker, 11,
+            Description = "Color of the RPM bar when in the redline zone",
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string RpmUpshiftColor { get; set; } = "#ff006a";
+    }
+}
