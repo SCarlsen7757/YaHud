5dfa7279e5b2e296ef07bb4918bc83cb6998e3dc
Mark Carlsen <mark@scarlsen.dk>
2025-10-23 13:35:06 +0200

    Update EnumExtensions for safer attribute access

--- R3E.YaHud/EnumExtensions.cs
+++ R3E.YaHud/EnumExtensions.cs
@@ -7,10 +7,15 @@ namespace R3E.YaHud
     {
         public static string GetDisplayName(this Enum enumValue)
         {
-            return enumValue.GetType()
-                            .GetMember(enumValue.ToString())[0]
-                            .GetCustomAttribute<DisplayAttribute>()?
-                            .Name ?? enumValue.ToString();
+            var members = enumValue.GetType().GetMember(enumValue.ToString());
+            
+            if (members.Length == 0)
+            {
+                return enumValue.ToString();
+            }
+
+            var displayAttribute = members[0].GetCustomAttribute<DisplayAttribute>();
+            return displayAttribute?.Name ?? enumValue.ToString();
         }
     }
 }
