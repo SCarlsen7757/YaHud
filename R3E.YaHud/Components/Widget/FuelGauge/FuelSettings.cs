64ae37505e665f067be0e943da4b8f67c38419f9
Oliver <Oliver.veile@gmail.com>
2025-10-26 20:43:00 +0100

    Made CSS and div's for all fuel elements Added dynamic colors for FR and FTE.

--- R3E.YaHud/Components/Widget/FuelGauge/FuelSettings.cs
+++ R3E.YaHud/Components/Widget/FuelGauge/FuelSettings.cs
@@ -5,15 +5,6 @@ namespace R3E.YaHud.Components.Widget.FuelGauge
 {
     public class FuelSettings : BasicSettings
     {
-        [SettingType("Fuel Green", SettingsTypes.ColorPicker, 10,
-            Description = "Color of the RPM bar",
-            ViewMode = SettingsViewMode.Intermediate)]
-        public string RpmColor { get; set; } = "#0069ff";
         
-
-        [SettingType("RPM Redline Color", SettingsTypes.ColorPicker, 11,
-            Description = "Color of the RPM bar when in the redline zone",
-            ViewMode = SettingsViewMode.Intermediate)]
-        public string RpmUpshiftColor { get; set; } = "#ff006a";
     }
 }
