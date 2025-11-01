80b1d6472a2dad1b8327c4aad586d7b16273515c
Mark Carlsen <mark@scarlsen.dk>
2025-11-02 00:08:21 +0100

    Refactor property handling and state updates
    Refactored `HudWidgetBase.cs` to simplify `StateHasChanged` invocation by removing redundant calls.
    
    Updated `UserInputsSettings.cs` to replace auto-implemented properties with properties backed by private fields. Added change detection and `NotifyPropertyChanged()` calls to improve encapsulation and ensure proper UI state updates.

--- R3E.YaHud/Components/Widget/UserInputs/UserInputsSettings.cs
+++ R3E.YaHud/Components/Widget/UserInputs/UserInputsSettings.cs
@@ -5,34 +5,102 @@ namespace R3E.YaHud.Components.Widget.UserInputs
 {
     public class UserInputsSettings : BasicSettings
     {
+        private bool showSteering = true;
+
         [SettingType("Show Steering input", SettingsTypes.Checkbox, 10,
             Description = "Show steering input",
             ViewMode = SettingsViewMode.Beginner)]
-        public bool ShowSteering { get; set; } = true;
+        public bool ShowSteering
+        {
+            get => showSteering;
+            set
+            {
+                if (value == showSteering) return;
+                showSteering = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string steeringColor = "#ffffff";
 
         [SettingType("Steering Color", SettingsTypes.ColorPicker, 11,
             Description = "Color of the steering input",
             ViewMode = SettingsViewMode.Expert)]
-        public string SteeringColor { get; set; } = "#ffffff";
+        public string SteeringColor
+        {
+            get => steeringColor;
+            set
+            {
+                if (value == steeringColor) return;
+                steeringColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        private bool showClutch = true;
 
         [SettingType("Show Clutch", SettingsTypes.Checkbox, 15,
             Description = "Show clutch input",
             ViewMode = SettingsViewMode.Intermediate)]
-        public bool ShowClutch { get; set; } = true;
+        public bool ShowClutch
+        {
+            get => showClutch;
+            set
+            {
+                if (value == showClutch) return;
+                showClutch = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string clutchColor = "#ffdc1c";
+
 
         [SettingType("Clutch Color", SettingsTypes.ColorPicker, 20,
             Description = "Color of the RPM bar",
             ViewMode = SettingsViewMode.Expert)]
-        public string ClutchColor { get; set; } = "#ffdc1c";
+        public string ClutchColor
+        {
+            get => clutchColor;
+            set
+            {
+                if (value == clutchColor) return;
+                clutchColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string throttleColor = "#1ea5ff";
 
         [SettingType("Throttle Color", SettingsTypes.ColorPicker, 21,
             Description = "Color of the RPM bar",
             ViewMode = SettingsViewMode.Expert)]
-        public string ThrottleColor { get; set; } = "#1ea5ff";
+        public string ThrottleColor
+        {
+            get => throttleColor;
+            set
+            {
+                if (value == throttleColor) return;
+                throttleColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string brakeColor = "#ff1e1e";
 
         [SettingType("Brake Color", SettingsTypes.ColorPicker, 22,
             Description = "Color of the RPM bar",
             ViewMode = SettingsViewMode.Expert)]
-        public string BrakeColor { get; set; } = "#ff1e1e";
+        public string BrakeColor
+        {
+            get => brakeColor;
+            set
+            {
+                if (value == brakeColor) return;
+                brakeColor = value;
+                NotifyPropertyChanged();
+            }
+        }
     }
 }
