28c3f738f3b6806c738419a8fb673db5f0c64fc7
Oliver <Oliver.veile@gmail.com>
2025-11-05 19:30:54 +0100

    Updated CSS for Fuel widget. Updated colors for texts Added FuelSetting colors so its easier to see the color

--- R3E.YaHud/Components/Widget/FuelGauge/FuelSettings.cs
+++ R3E.YaHud/Components/Widget/FuelGauge/FuelSettings.cs
@@ -5,6 +5,9 @@ namespace R3E.YaHud.Components.Widget.FuelGauge
 {
     public class FuelSettings : BasicSettings
     {
-        
+        public string red { get; set; } = "rgb(255, 0, 0)";
+        public string yellow { get; set; } = "rgb(255, 251, 0)";
+        public string orange { get; set; } = "rgb(255, 102, 0)";
+        public string green { get; set; } = "rgb(0, 128, 0)";
     }
 }
