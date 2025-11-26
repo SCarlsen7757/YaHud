d2e50c87ae1e41807ae5a840e5294a910547a028
ADoessing <32567122+ADoessing@users.noreply.github.com>
2025-11-26 19:30:09 +0100

    Initial

---
+++ R3E.YaHud/Components/Widget/Radar/RadarSettings.cs
@@ -0,0 +1,14 @@
+﻿using R3E.YaHud.Components.Widget.Core;
+using R3E.YaHud.Services.Settings;
+
+
+namespace R3E.YaHud.Components.Widget.Radar
+{
+    public class RadarSettings : BasicSettings
+    {
+        public int RadarRange { get; set; } = 12;
+        public float RadarOpacity { get; set; } = 0.8f;
+        public int RadarFadeRange { get; set; } = 2;
+        public string RadarGridSvgPath => $"/img/radar/radar-grid.png";
+    }
+}
