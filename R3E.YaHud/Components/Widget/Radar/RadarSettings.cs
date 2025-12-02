273e397dcb10f3f1a6dbb565e9e488e3a9296b0a
ADoessing <32567122+ADoessing@users.noreply.github.com>
2025-12-02 21:43:29 +0100

    Updated some styling handling and made the radar fade out when no opponents are close

--- R3E.YaHud/Components/Widget/Radar/RadarSettings.cs
+++ R3E.YaHud/Components/Widget/Radar/RadarSettings.cs
@@ -6,9 +6,12 @@ namespace R3E.YaHud.Components.Widget.Radar
 {
     public class RadarSettings : BasicSettings
     {
+        [SettingType("Radar rage", SettingsTypes.Number, 1, Description = "Range of the radar to start detecting opponents in meters", ViewMode =SettingsViewMode.Expert, Max = 80, Min = 1)]
         public int RadarRange { get; set; } = 12;
         public float RadarOpacity { get; set; } = 0.8f;
         public int RadarFadeRange { get; set; } = 2;
-        public string RadarGridSvgPath => $"assets/img/radar/radar-grid.png";
+
+        [SettingType("Auto hide radar", SettingsTypes.Checkbox, 0,Description ="Hides the radar if no opponents are within radar range", ViewMode = SettingsViewMode.Beginner)]
+        public bool AutoHideRadar { get; set; } = true;
     }
 }
