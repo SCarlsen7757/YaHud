858c2a1c3544368f69f5b893b170467a9818dda3
Oliver <Oliver.veile@gmail.com>
2026-01-03 19:22:12 +0100

    Refractored FuelToAdd in FuelCalc.razor. Removed f from 0.0 since they are not needed. Updated FuelSettings.cs with colorpicker and setting page. Added dispose for Fueldata in TelemetryService.cs.

--- R3E.YaHud/Components/Widget/FuelCalc/FuelSettings.cs
+++ R3E.YaHud/Components/Widget/FuelCalc/FuelSettings.cs
@@ -1,22 +1,46 @@
 ﻿using R3E.YaHud.Components.Widget.Core;
+using R3E.YaHud.Services.Settings;
 
 namespace R3E.YaHud.Components.Widget.FuelCalc
 {
     public class FuelSettings : BasicSettings
     {
-        private const string Red  = "rgb(255, 0, 0)";
-        private const string Yellow  = "rgb(255, 251, 0)";
-        private const string Orange = "rgb(255, 102, 0)";
-        private const string Green  = "rgb(0, 128, 0)";
-        private const string DimmedYellow = "rgb(127, 127, 43)";
-        public string FuelPerLapColor =>  DimmedYellow;
-        public string LapsEstimatedLeftColor =>  Green;
-        public string FuelToAddColor => Green;
-        public string LastLapFuelUsageColor => DimmedYellow;
-        public string ColorHigh => Green;
-        public string ColorMidHigh => Yellow;
-        public string ColorMidLow => Orange;
-        public string ColorLow => Red;
-
+        private const string Red  = "#FF0000";
+        private const string Yellow  = "#FFFF00";
+        private const string Orange = "#FF6600";
+        private const string Green  = "#008000";
+        private const string DimmedYellow = "#7F7F2B";
+        
+        [SettingType("FuelPerLapColor", SettingsTypes.ColorPicker, 10,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string FuelPerLapColor { get; set; } = DimmedYellow;
+        
+        [SettingType("LapsEstimatedLeftColor", SettingsTypes.ColorPicker, 11,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string LapsEstimatedLeftColor { get; set; } = Green;
+        
+        [SettingType("FuelToAddColor", SettingsTypes.ColorPicker, 12,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string FuelToAddColor { get; set; } = Green;
+        
+        [SettingType("LastLapFuelUsageColor", SettingsTypes.ColorPicker, 13,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string LastLapFuelUsageColor { get; set; } = DimmedYellow;
+        
+        [SettingType("ColorHigh", SettingsTypes.ColorPicker, 14,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string ColorHigh { get; set; } = Green; 
+        
+        [SettingType("ColorMidHigh", SettingsTypes.ColorPicker, 15,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string ColorMidHigh { get; set; } = Yellow;
+        
+        [SettingType("ColorMidLow", SettingsTypes.ColorPicker, 16,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string ColorMidLow { get; set; } = Orange;
+        
+        [SettingType("ColorLow", SettingsTypes.ColorPicker, 17,
+            ViewMode = SettingsViewMode.Intermediate)]
+        public string ColorLow { get; set; } = Red;
     }
 }
