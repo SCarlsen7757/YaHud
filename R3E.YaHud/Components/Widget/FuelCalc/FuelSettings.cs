ed0b341710c38e146037a249c1a528406d5a0630
Oliver <Oliver.veile@gmail.com>
2026-01-04 10:39:49 +0100

    Added space between Fuel and Calc in FuelCalc.razor. Changed all Colors defined in FuelCalc.razor from rgb to hex code. Added space in setting name in FuelSettings.cs.

--- R3E.YaHud/Components/Widget/FuelCalc/FuelSettings.cs
+++ R3E.YaHud/Components/Widget/FuelCalc/FuelSettings.cs
@@ -11,35 +11,35 @@ namespace R3E.YaHud.Components.Widget.FuelCalc
         private const string Green  = "#008000";
         private const string DimmedYellow = "#7F7F2B";
         
-        [SettingType("FuelPerLapColor", SettingsTypes.ColorPicker, 10,
+        [SettingType("Fuel Per Lap Color", SettingsTypes.ColorPicker, 10,
             ViewMode = SettingsViewMode.Intermediate)]
         public string FuelPerLapColor { get; set; } = DimmedYellow;
         
-        [SettingType("LapsEstimatedLeftColor", SettingsTypes.ColorPicker, 11,
+        [SettingType("Laps Estimated Left Color", SettingsTypes.ColorPicker, 11,
             ViewMode = SettingsViewMode.Intermediate)]
         public string LapsEstimatedLeftColor { get; set; } = Green;
         
-        [SettingType("FuelToAddColor", SettingsTypes.ColorPicker, 12,
+        [SettingType("Fuel To Add Color", SettingsTypes.ColorPicker, 12,
             ViewMode = SettingsViewMode.Intermediate)]
         public string FuelToAddColor { get; set; } = Green;
         
-        [SettingType("LastLapFuelUsageColor", SettingsTypes.ColorPicker, 13,
+        [SettingType("Last Lap Fuel Usage Color", SettingsTypes.ColorPicker, 13,
             ViewMode = SettingsViewMode.Intermediate)]
         public string LastLapFuelUsageColor { get; set; } = DimmedYellow;
         
-        [SettingType("ColorHigh", SettingsTypes.ColorPicker, 14,
+        [SettingType("Color High", SettingsTypes.ColorPicker, 14,
             ViewMode = SettingsViewMode.Intermediate)]
         public string ColorHigh { get; set; } = Green; 
         
-        [SettingType("ColorMidHigh", SettingsTypes.ColorPicker, 15,
+        [SettingType("Color Mid High", SettingsTypes.ColorPicker, 15,
             ViewMode = SettingsViewMode.Intermediate)]
         public string ColorMidHigh { get; set; } = Yellow;
         
-        [SettingType("ColorMidLow", SettingsTypes.ColorPicker, 16,
+        [SettingType("Color Mid Low", SettingsTypes.ColorPicker, 16,
             ViewMode = SettingsViewMode.Intermediate)]
         public string ColorMidLow { get; set; } = Orange;
         
-        [SettingType("ColorLow", SettingsTypes.ColorPicker, 17,
+        [SettingType("Color Low", SettingsTypes.ColorPicker, 17,
             ViewMode = SettingsViewMode.Intermediate)]
         public string ColorLow { get; set; } = Red;
     }
