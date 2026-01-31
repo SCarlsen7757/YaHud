258706259b8c2dfce7a885d167fa8b2b6c1abc37
Oliver <Oliver.veile@gmail.com>
2026-01-31 17:30:29 +0100

    Made a applet icon for Linux! Tested quickly and works. Using our favicon i have imported from R3E.Yahud/wwwroot/favicon.png

--- R3E.Tray/Program.cs
+++ R3E.Tray/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+namespace R3E.Tray;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Console.WriteLine("Using Linux");
+            Linux.LinuxTray.Run();
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Console.WriteLine("Using Windows");
+            //Windows.WindowsTray.Run();
+            return;
+        }
+
+        Console.WriteLine("Unsupported OS");
+    }
+}
